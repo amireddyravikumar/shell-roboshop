4719702fbc9095968263ee2062d973f55495a1cd
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-30 00:07:18 +0530

    shell roboshop

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -31,5 +31,12 @@ VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOG_FILE
 VALIDATE $? "Installing Mongo DB"
+
 systemctl enable --now mongod 
 VALIDATE $? "starting and enabling Mongo DB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+VALIDATE $? "Allowing remote connection to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB"
