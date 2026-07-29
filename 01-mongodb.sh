f275173a92e7e742b28ac5369f7278cb879c42c6
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 23:40:08 +0530

    shell roboshop

---
+++ 01-mongodb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOG_FILE="$LOGS_FOLDER/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[31m"
+Y="\e[31m"
+N="\e[0m"
+
+if [ USERID -ne 0]; then
+    echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
+    exit 1
+fi
+function VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2...  $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else 
+        echo -e "$TIMESTAMP [INFO] $2... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo" | tee -a $LOG_FILE
