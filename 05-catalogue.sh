dfe809903731fbb3677f4b663adaa20f08cfec11
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-30 18:17:35 +0530

    roboshop shell

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -31,7 +31,7 @@ dnf module enable nodejs:20 -y &>>$LOG_FILE
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS:20"
 
-id roboshop &>>$LOGS_FILE
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
