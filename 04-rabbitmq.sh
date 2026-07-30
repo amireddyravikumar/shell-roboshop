93759f2b1acda0e3c1f8d6814203356784581881
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-30 16:22:56 +0530

    roboshop shell

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -4,39 +4,37 @@ LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+LOG_FILE="$LOGS_FOLDER/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    echo -e "$R Please run with root access $N" | tee -a $LOG_FILE
     exit 1
 fi
-
-VALIDATE(){
+function VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2...  $R FAILED $N" | tee -a $LOG_FILE
         exit 1
-    else
-        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    else 
+        echo -e "$TIMESTAMP [INFO] $2... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-VALIDATE $? "Adding rabbitmq repo"
+VALIDATE $? "Adding RabbitMQ Repo"
 
-dnf install rabbitmq-server -y &>> $LOGS_FILE
-VALIDATE $? "Installing rabbitmq server"
+dnf install rabbitmq-server -y &>> $LOG_FILE
+VALIDATE $? "Installing RabbitMQ Server"
 
-systemctl enable rabbitmq-server &>> $LOGS_FILE
-systemctl start rabbitmq-server &>> $LOGS_FILE
-VALIDATE $? "Enabling and starting rabbitmq server"
+systemctl enable rabbitmq-server &>> $LOG_FILE
+systemctl start rabbitmq-server &>> $LOG_FILE
+VALIDATE $? "Starting and enabling RabbitMQ Server"
 
-rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
-VALIDATE $? "setting up username and password"
+rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOG_FILE
+VALIDATE $? "settng uo the user and password for RabbitMQ Server"
